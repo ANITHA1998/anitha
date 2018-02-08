815b15580baf7dd572d5ef2b87b5d3f0bd1adc07
ANITHA1998 <36070554+ANITHA1998@users.noreply.github.com>
2018-02-08 09:54:23 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<conio.h>
+void main()
+{
+    int i, n;
+    cin >> n;
+    for (i=0; i<n; i++)
+    {
+        cout << "HELLO \n";
+    }
+    getch();
+    return 0;
+}
